681c7a711c34b4ec752ff9fa8f13d81769b70209
Aaron Reynoza <ldaaron38@gmail.com>
2020-06-22 22:16:00 -0600

    modify scripts

--- init/onboard-msisdn-oracle/insert_msisdns.sh
+++ init/onboard-msisdn-oracle/insert_msisdns.sh
@@ -1,14 +1,21 @@
-#! /bin/bash
+#!/bin/sh
+
+"Starting flow for ${MSISDN_LIST}"
 
 CURRENT_DATE=$(date)
 for participantMSISDN in $(echo "${MSISDN_LIST}" | jq -c '.[]'); do
-    echo "Calling Pathfinder with ${participantMSISDN}";
     MSISDN=$(echo "${participantMSISDN}" | jq -r '.MSISDN');
-    CURRENCY=$(echo "${participantMSISDN}" | jq -c '.currency');
+    CURRENCY=$(echo "${participantMSISDN}" | jq -r '.currency');
     DATA="{currency: \"${CURRENCY}\", fspId: \"${SIM_NAME}\"}"
+    echo "Making a request for ${participantMSISDN}"
+    echo "Pathfinder URl is ${PATHFINDER_ORACLE_HOST}/participants/MSISDN/${MSISDN}"
+    echo "Current date is ${CURRENT_DATE}"
+    echo "Data is ${DATA}"
     curl -v --location --request PUT "${PATHFINDER_ORACLE_HOST}/participants/MSISDN/${MSISDN}" \
     --header 'Content-Type: application/json' \
     --header "date: ${CURRENT_DATE}" \
     --header "fspiop-source: ${SIM_NAME}" \
     --data-raw "${DATA}";
 done
+
+echo "MSISDNS have been onboarded"
